1d2d92c1cc21acfaaf48dc202ab6f2f09eee30de
Aaron Gorka <aaron.gorka@amaysim.com.au>
2018-03-23 15:33:57 +1100

    Multithreading

--- start.py
+++ start.py
@@ -3,21 +3,27 @@ from multiprocessing.dummy import Pool as ThreadPool
 from flask import Flask,jsonify,request
 import datetime
 import json
+from multiprocessing import Pool
+from multiprocessing import cpu_count
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
-    return_object = f(10)
+    processes = cpu_count()
+    pool = ThreadPool(processes)
+    return_object = pool.starmap(f, zip(list(range(processes))))
+    pool.close()
+    pool.join()
     return json.dumps(return_object)
 
 def f(x):
     start_time = datetime.datetime.now()
     elapsed_time = datetime.datetime.now() - start_time
+    x = 213123
     while elapsed_time.microseconds < 1000000:
         elapsed_time = datetime.datetime.now() - start_time
-        x = 213123
-        x*x
+        x = x*x
         x = x + 1
     return 'Done!'
 
